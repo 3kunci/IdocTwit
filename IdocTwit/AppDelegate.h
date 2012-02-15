3971d9cf3be0a33c4e8cdda9ec08062c4f7790cb
Ikhsan Assaat <ixnixnixn@yahoo.com>
2012-02-15 16:11:30 +0700

    Initial Commit

---
+++ IdocTwit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IdocTwit
+//
+//  Created by Ikhsan Assaat on 2/15/12.
+//  Copyright (c) 2012 3kunci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IdocTwit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IdocTwit
+//
+//  Created by Ikhsan Assaat on 2/15/12.
+//  Copyright (c) 2012 3kunci. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
